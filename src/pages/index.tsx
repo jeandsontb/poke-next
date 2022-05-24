379f38821b64ab6a48ed1f713939d4597327174b
jeandson <jeandsontb@gmail.com>
2022-05-23 21:57:12 -0300

    implemented cards in application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { NextPage, GetStaticProps } from "next";
+import { Card, Grid, Row, Text } from "@nextui-org/react";
+
 import { Layout } from "../components/layout";
 import pokeApi from "../api/pokeApi";
 import { PokemonListResponse, SmallPokemon } from "../interfaces";
@@ -10,15 +12,26 @@ interface Props {
 const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Listagem de Pokemons">
-      <>
+      <Grid.Container gap={2} justify="flex-start">
         <ul>
-          {pokemons.map(({ id, name }) => (
-            <li key={id}>
-              #{id} - {name}
-            </li>
+          {pokemons.map(({ id, name, img }) => (
+            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+              <Card hoverable clickable>
+                <Card.Body css={{ p: 1 }}>
+                  <Card.Image src={img} width="100%" height={140} />
+                </Card.Body>
+
+                <Card.Footer>
+                  <Row justify="space-between">
+                    <Text transform="capitalize">{name}</Text>
+                    <Text>#{id}</Text>
+                  </Row>
+                </Card.Footer>
+              </Card>
+            </Grid>
           ))}
         </ul>
-      </>
+      </Grid.Container>
     </Layout>
   );
 };
