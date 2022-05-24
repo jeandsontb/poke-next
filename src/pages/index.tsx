e6d13f657fdf70ab25d15b0a9473e116edcd562c
jeandson <jeandsontb@gmail.com>
2022-05-24 08:26:12 -0300

    implements component pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage, GetStaticProps } from "next";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Grid } from "@nextui-org/react";
 
 import { Layout } from "../components/layout";
 import pokeApi from "../api/pokeApi";
 import { PokemonListResponse, SmallPokemon } from "../interfaces";
+import { PokemonCard } from "../components/pokemon";
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -13,24 +14,9 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title="Listagem de Pokemons">
       <Grid.Container gap={2} justify="flex-start">
-        <ul>
-          {pokemons.map(({ id, name, img }) => (
-            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-              <Card hoverable clickable>
-                <Card.Body css={{ p: 1 }}>
-                  <Card.Image src={img} width="100%" height={140} />
-                </Card.Body>
-
-                <Card.Footer>
-                  <Row justify="space-between">
-                    <Text transform="capitalize">{name}</Text>
-                    <Text>#{id}</Text>
-                  </Row>
-                </Card.Footer>
-              </Card>
-            </Grid>
-          ))}
-        </ul>
+        {pokemons.map((pokemon) => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
       </Grid.Container>
     </Layout>
   );
@@ -39,7 +25,7 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
 export default HomePage;
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=156");
 
   const pokemons: SmallPokemon[] = data.results.map((poke, index) => ({
     ...poke,
